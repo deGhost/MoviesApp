837091028330599ff69987ec27ee1198930439b0
H. Sara <monome7@gmail.com>
2022-02-19 02:16:35 +0100

    Adding comments

--- app/src/main/java/com/sarali/moviesapp/MovieOverview.java
+++ app/src/main/java/com/sarali/moviesapp/MovieOverview.java
@@ -19,19 +19,29 @@ public class MovieOverview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_overview);
+        
+        
         ImageView ivPoster;
         TextView tvTitle, tvReleaseDate;
         TextView tvOverview;
 
+        // Retriving movie object passed from MainActivity
         Movie selectedMovie = getIntent().getParcelableExtra("overview");
+        
+        // Retriving views from layout xml
         ivPoster = findViewById(R.id.ivPoster);
         tvTitle = findViewById(R.id.tvTitle);
         tvReleaseDate= findViewById(R.id.tvReleaseDate);
         tvOverview = findViewById(R.id.tvOverview);
+        
+        // Setting views with data contained in the passed movie object
         tvTitle.setText(selectedMovie.getTitle());
         tvReleaseDate.setText(selectedMovie.getReleaseDate());
         tvOverview.setText(selectedMovie.getOverview());
+        
         Log.d(TAG, "onCreate: selectedMovie.getPosterPath() "+selectedMovie.getPosterPath());
+        
+        // Displaying poster image using Glide library
         Glide.with(this)
                 .asBitmap()
                 .load(selectedMovie.getPosterPath())
